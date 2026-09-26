67ec201c023075a3dd05f388db485884a047ca96
wgtkkf <takuro.tokunaga@utah.edu>
2026-09-25 22:56:48 -0700

    updated source.cpp

--- cpp/include/Parameter.hpp
+++ cpp/include/Parameter.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include <string>
 
+#include <cstddef> // for std::size_t
+
 class Input{
   public:
-    int size0 = 3;
+    static constexpr std::size_t size0 = 3;
+    static constexpr std::size_t size1 = 2;
+    
     std::string config_name{""};
 
     // constructor
